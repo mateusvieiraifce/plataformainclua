19ccbad6326f66d91ff9e59e04eef212cf3385c4
Anthony <educardobarros@gmail.com>
2024-05-07 16:02:57 -0300

    Alteração para password null

---
+++ codificação/inclua/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vendas;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function home()
+    {
+        $usuario = Auth::user();
+        $compras = Vendas::where('comprador_id', $usuario->id)->orderBy('created_at', 'desc')->get();
+
+        return view('dashboard', ['compras'=>$compras]);
+    }
+}
