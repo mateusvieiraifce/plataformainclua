44d45d4c0fe8623fe5742806829820e6e1839429
anthonyeduardob <educardobarros@gmail.com>
2024-07-02 10:46:15 -0300

    Correção de login para renovar assinatura

--- codificação/inclua/app/Http/Middleware/VerifyPaymentSignature.php
+++ codificação/inclua/app/Http/Middleware/VerifyPaymentSignature.php
@@ -20,19 +20,21 @@ class VerifyPaymentSignature
     public function handle(Request $request, Closure $next)
     {
         $user = User::where('email', $request->email)->first();
-        $assinatura = Assinatura::where('user_id', $user->id)->first();
-        if ($assinatura->status == "RENOVAÇÂO PENDENTE" && $assinatura->situacao == "CANCELADA") {
-            $assinaturaController = new AssinaturaController();
-            $response = $assinaturaController->renovacaoManual($assinatura->id);
-            
-            return $response;
-        } elseif ($assinatura->status == "NEGADA" && $assinatura->situcao == "CANCELADA") {
-            dd('direcionar para o formulario');
-        } elseif ($assinatura->status == "APROVADA" && $assinatura->situcao == "ATIVA") {
-            return $next($request);
+
+        if ($user->tipo_user == "P") {   
+             $assinatura = Assinatura::where('user_id', $user->id)->first();
+            if ($assinatura->status == "RENOVAÇÂO PENDENTE" && $assinatura->situacao == "CANCELADA") {
+                $assinaturaController = new AssinaturaController();
+                $response = $assinaturaController->renovacaoManual($assinatura->id);
+                
+                return $response;
+            } elseif ($assinatura->status == "NEGADA" && $assinatura->situcao == "CANCELADA") {
+                dd('direcionar para o formulario');
+            } elseif ($assinatura->status == "APROVADA" && $assinatura->situcao == "ATIVA") {
+                return $next($request);
+            }
         }
-    
-        session()->flash('msg', ['valor' => trans("Não foi possível renovar sua assinatura."), 'tipo' => 'danger']);
-        return back();
+
+        return $next($request);
     }
 }
