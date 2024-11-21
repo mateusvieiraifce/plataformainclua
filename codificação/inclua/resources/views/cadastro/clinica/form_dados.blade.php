16c359c617bafb7da64e4cb1ed7e03f69aaf4bb2
mentrixmax <mentrixmax@gmail.com>
2024-11-21 13:53:20 -0300

    Corrigindo problemas do samuel

--- codificação/inclua/resources/views/cadastro/clinica/form_dados.blade.php
+++ codificação/inclua/resources/views/cadastro/clinica/form_dados.blade.php
@@ -32,7 +32,7 @@
                             </label>
                             <div class="input-group input-medium{{ $errors->has('documento') ? ' has-danger' : '' }}">
                                 <input type="text" id="documento" class="form-control border-full {{ $errors->has('documento') ? 'is-invalid' : '' }}"
-                                    name="documento" maxlength="18" placeholder="00.000.000/0000-00" oninput="mascaraCnpj(this)" onblur="consultarCNPJ(this)
+                                    name="documento" maxlength="18" placeholder="00.000.000/0000-00" oninput="mascaraCnpj(this)" onblur="consultarCNPJ(this)"
                                     value="{{ (isset($clinica) && $clinica->cnpj ? $clinica->cnpj : null) ?? old('documento') }}" required>
                                 @include('alerts.feedback', ['field' => 'documento'])
                             </div>
@@ -48,7 +48,7 @@
                                 @include('alerts.feedback', ['field' => 'nome_fantasia'])
                             </div>
                         </div>
-                        
+
                         <div class="form-group">
                             <label for="razao_social">
                                 Razão Social <span class="required">*</span>
@@ -95,7 +95,7 @@
                             </div>
                         </div>
 
-                        
+
                         <div class="form-group">
                             <label for="anamnese_obrigatoria">
                                 A Anamnese será obrigatória? <span class="required">*</span>
