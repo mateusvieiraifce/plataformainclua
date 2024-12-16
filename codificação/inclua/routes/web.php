4e5ab2d22a921415a85f8f8da544b06422600029
sLdagama <163679705+sLdagama@users.noreply.github.com>
2024-12-16 08:22:06 -0300

    Remover rotaas não usadas no projeto Inclua

--- codificação/inclua/routes/web.php
+++ codificação/inclua/routes/web.php
@@ -31,8 +31,6 @@ Route::get("/", [\App\Http\Controllers\LandingPageController::class, 'index'])->
 Route::get("/login", [\App\Http\Controllers\UsuarioController::class, 'index'])->name('index');
 Route::any("/sendmailback", [\App\Http\Controllers\MailController::class, 'sendEmailBack'])->name('sendMailBack');
 
-Route::get('/checkout', [\App\Http\Controllers\CheckoutControler::class, "checkout"])->name('finalizar');
-
 Route::post('/mail', [\App\Http\Controllers\MailController::class, "sendMail"])->name('sendmail');
 
 #URL's AUTH GOOGLE
@@ -96,15 +94,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'home'])->name('home')->middleware('verify.signature');
     Route::get('/dashboard/clinica', [\App\Http\Controllers\DashboardController::class, 'dashboardClinica'])->name('dashboard.dashboardClinica');
 
-    Route::get("/turnvendedor", [\App\Http\Controllers\UsuarioController::class, 'turnVendedor'])->name('user.turnvendedor');
-
-    #USERS
-    Route::get("/user/comentarios", [\App\Http\Controllers\UsuarioController::class, 'comentariosComprador'])->name('user.comentarios');
-    Route::get("/user/compras", [\App\Http\Controllers\UsuarioController::class, 'compras'])->name('user.compras');
-    Route::get("/user/favoritos", [\App\Http\Controllers\UsuarioController::class, 'listFavoritos'])->name('user.favoritos');
-    Route::get("/user/notificacoes", [\App\Http\Controllers\UsuarioController::class, 'listNotificacoes'])->name('user.notificacoes');
-    Route::get("/user/notificacao/{id}", [\App\Http\Controllers\UsuarioController::class, 'lerNotificacoes'])->name('user.notificacoes.ler');
-
     #PROFILE
     Route::get("/profile/{id?}", [\App\Http\Controllers\UsuarioController::class, 'perfil'])->name('user.perfil');
     Route::post("/profile/update", [\App\Http\Controllers\UsuarioController::class, 'updateUser'])->name('user.update');
@@ -119,54 +108,6 @@ Route::middleware('auth')->group(function () {
     Route::get("/profile/endereco/edit/{id}", [\App\Http\Controllers\EnderecoController::class, 'edit'])->name('user.endereco.edit');
     Route::get("/profile/endereco/principal/{id}", [\App\Http\Controllers\EnderecoController::class, 'setEnderecoPrincipal'])->name('user.endereco.principal');
 
-    #ADVERTISEMENT
-    Route::get("/advertisement", [\App\Http\Controllers\AnuncioController::class, 'list'])->name('advertisement.list');
-    Route::get("/advertisement/add", [\App\Http\Controllers\AnuncioController::class, 'add'])->name('advertisement.add');
-    Route::post("/advertisement/save", [\App\Http\Controllers\AnuncioController::class, 'save'])->name('advertisement.save');
-    Route::post("/advertisement/preco", [\App\Http\Controllers\AnuncioController::class, 'passo2'])->name('advertisement.preco');
-    Route::get("/advertisement/preco/{id}", [\App\Http\Controllers\AnuncioController::class, 'passo1'])->name('advertisement.back');
-    Route::get("/advertisement/precoback/{id}", [\App\Http\Controllers\AnuncioController::class, 'passo2back'])->name('advertisement.back.fotos');
-    Route::any("/advertisement/fotos", [\App\Http\Controllers\AnuncioController::class, 'passo3'])->name('advertisement.fotos');
-    Route::any("/advertisement/fotos/{id}", [\App\Http\Controllers\AnuncioController::class, 'passoFotos'])->name('advertisement.passo.fotos');
-    Route::any("/advertisement/finalizar/", [\App\Http\Controllers\AnuncioController::class, 'addFotos'])->name('advertisement.finalizar');
-    Route::get("/advertisement/destacar/{id}", [\App\Http\Controllers\AnuncioController::class, 'destacar'])->name('advertisement.destacar');
-    Route::post("/advertisement/destacar/{id}", [\App\Http\Controllers\AnuncioController::class, 'destacarDo'])->name('advertisement.destacar.do');
-    Route::get("/advertisement/tamanho/{id}", [\App\Http\Controllers\AnuncioController::class, 'tamanho'])->name('advertisement.tamanho');
-    Route::post("/advertisement/tamanho/add/{id}", [\App\Http\Controllers\AnuncioController::class, 'tamanhoAdd'])->name('advertisement.tamanho.add');
-    Route::get("/advertisement/tamanho/del/{id}", [\App\Http\Controllers\AnuncioController::class, 'deleteTamanho'])->name('advertisement.tamanho.del');
-    Route::get("/advertisement/tamanho/edit/{id}", [\App\Http\Controllers\AnuncioController::class, 'editTamanho'])->name('advertisement.tamanho.edit');
-    Route::get("/advertisement/fim/{id}", [\App\Http\Controllers\AnuncioController::class, 'finalizar'])->name('advertisement.tamanho.finalizar');
-    Route::get("/advertisement/delete/{id}", [\App\Http\Controllers\AnuncioController::class, 'delete'])->name('advertisement.delete');
-    Route::get("/advertisement/edit/{id}", [\App\Http\Controllers\AnuncioController::class, 'edit'])->name('advertisement.edit');
-
-    #FAVORITE
-    Route::get("/favorite/add/{id}", [\App\Http\Controllers\AnuncioController::class, 'addFavorite'])->name('advertisement.addfavorito');
-    Route::get("/favorite/list", [\App\Http\Controllers\AnuncioController::class, 'listFavorite'])->name('advertisement.listfavorito');
-    Route::get("/favorite/remove/{id}", [\App\Http\Controllers\AnuncioController::class, 'remFavorite'])->name('advertisement.remfavorito');
-
-    /*TODO REFACTORY TO FRONT */
-    Route::get("/detail/{id}", [\App\Http\Controllers\AnuncioController::class, 'produtctDetail'])->name('advertisement.detail');
-    Route::post("/comentario/add/", [\App\Http\Controllers\AnuncioController::class, 'addComentario'])->name('advertisement.comentario.add');
-
-    #CART
-    Route::get("/cart/add/", [\App\Http\Controllers\AnuncioController::class, 'addSession'])->name('advertisement.addsession');
-    Route::get("/cart/view/", [\App\Http\Controllers\AnuncioController::class, 'viewSession'])->name('advertisement.viewssesion');
-    Route::any("/cart/clear/", [\App\Http\Controllers\AnuncioController::class, 'clearCarr'])->name('cart.clear');
-    Route::get("/cart/remqnt/{id}", [\App\Http\Controllers\CheckoutControler::class, 'removerQntCarrinho'])->name('cart.remqtd');
-    Route::get("/cart/addqnt/{id}", [\App\Http\Controllers\CheckoutControler::class, 'addQntCarrinho'])->name('cart.addqtd');
-
-    #CHECK OUT
-    Route::post("/checkout/create", [\App\Http\Controllers\CheckoutControler::class, 'create'])->name('vendas.create');
-    Route::get("/checkout/address", [\App\Http\Controllers\CheckoutControler::class, 'addEndereco'])->name('vendas.adr.create');
-    Route::post("/checkout/address/save", [\App\Http\Controllers\CheckoutControler::class, 'saveEndereco'])->name('vendas.adr.save');
-    Route::get("/checkout/confirmpay/{id}", [\App\Http\Controllers\CheckoutControler::class, 'posProcessPagamento'])->name('vendas.payment');
-    Route::get("/checkout/useraddress/{id?}", [\App\Http\Controllers\UsuarioController::class, 'findAdress'])->name('vendas.endereco');
-    Route::any("/checkout/confirmpay", [\App\Http\Controllers\CheckoutControler::class, 'returnPagSeguro'])->name('vendas.payment.return');
-
-    #SALES
-    Route::get("/sales/list", [\App\Http\Controllers\VendasController::class, 'list'])->name('sales.list');
-    Route::get("/sales/send/{id}", [\App\Http\Controllers\VendasController::class, 'send'])->name('sales.send');
-    Route::post("/sales/send/do/{id}", [\App\Http\Controllers\VendasController::class, 'sendDo'])->name('sales.send.do');
     Route::get("/send/mail", [\App\Http\Controllers\MailController::class, 'sendMenssagem'])->name('sales.send.do.email');
 
     #ESPECIALIDADES
