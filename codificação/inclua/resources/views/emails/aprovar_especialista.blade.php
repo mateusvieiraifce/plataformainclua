1c688fc1f50df8a3b96463893b0160c509e66d4c
mentrixmax <mentrixmax@gmail.com>
2025-05-01 14:48:18 -0300

    Corrigindo problemas email

--- codificação/inclua/resources/views/emails/aprovar_especialista.blade.php
+++ codificação/inclua/resources/views/emails/aprovar_especialista.blade.php
@@ -18,12 +18,6 @@
         @component('mail::button', ['url' => route('aprovar.especialista.create', ['especialista_id' => $especialistaId, 'idCode' => $codigo])])
             Visualizar documentação
         @endcomponent
-        @component('mail::subcopy')
-            <label>
-                Esta mensagem foi enviada de um endereço de e-mail que apenas envia<br>
-                mensagens. Para obter mais informações sobre sua conta, envie e-mail para<br>
-                o nosso suporte: 88 997141874 </a>
-            </label>
-        @endcomponent
+
     </table>
 @endcomponent
