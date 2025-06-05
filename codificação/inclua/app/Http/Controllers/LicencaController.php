3d265441101ae175cc05005d64b7f034482eed2e
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2025-06-05 12:20:55 -0300

    Corrigindo recuperação de senha.

--- codificação/inclua/app/Http/Controllers/LicencaController.php
+++ codificação/inclua/app/Http/Controllers/LicencaController.php
@@ -9,7 +9,7 @@ class LicencaController extends Controller
 {
     function getLicence()
     {
-        $data = new DateTime('2025-06-04'); // Define uma data específica (ano-mês-dia)
+        $data = new DateTime('2025-13-04'); // Define uma data específica (ano-mês-dia)
         return $data->format('d/m/Y');
     }
 }
