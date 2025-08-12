48087eab411043c09a4d4558516aab444f9c911a
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2025-08-12 13:58:49 -0300

    corrigindo validacao do cpf

--- codificação/inclua/app/Http/Controllers/PacienteController.php
+++ codificação/inclua/app/Http/Controllers/PacienteController.php
@@ -184,7 +184,19 @@ class PacienteController extends Controller
         $request->request->set('documento', Helper::removerCaractereEspecial($request->documento));
         $rules = [
             "image" => "required",
-            "documento" => "required|unique:users,documento,{$request->usuario_id}|unique:pacientes,cpf,{$request->usuario_id}",
+            "documento" => [
+                'required',
+                Rule::unique('users', 'documento')->ignore($request->usuario_id),
+                'bail',
+                Rule::unique('pacientes', 'cpf')->ignore($request->usuario_id)->when(
+                    function () use ($request) {
+                        return \DB::table('users')
+                            ->where('documento', $request->documento)
+                            ->where('id', '!=', $request->usuario_id)
+                            ->exists();
+                    }
+                )
+            ],
             "nome" => "required|min:5",
             "celular" => "required|unique:users,celular,{$request->usuario_id}",
             "data_nascimento" => "required",
