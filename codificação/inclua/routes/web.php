7a7148f812102293bf65c1286f615c993859d4d1
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2025-08-12 20:02:11 -0300

    subindo

--- codificação/inclua/routes/web.php
+++ codificação/inclua/routes/web.php
@@ -99,7 +99,7 @@ Route::post("/aprovar/especialista/store", [\App\Http\Controllers\ValidacoesCont
 Route::post("/auth/user", [\App\Http\Controllers\UsuarioController::class, 'logar'])->name('login.do');
 Route::get("/logout", [\App\Http\Controllers\UsuarioController::class, 'logout'])->name('logout');
 Route::get("/recuperar", [\App\Http\Controllers\UsuarioController::class, 'recover'])->name('recover');
-Route::get("/recuperar/{id?}", [\App\Http\Controllers\UsuarioController::class, 'recoverID'])->name('recover.mail');
+Route::get("/recuperar/{id?}", [\App\Http\Controllers\UsuarioController::class, 'recoverID'])->name('recover.mail')->middleware("check.domain");
 Route::post("/recuperar", [\App\Http\Controllers\UsuarioController::class, 'recoverDo'])->name('recover.do');
 Route::post("/updatepassword", [\App\Http\Controllers\UsuarioController::class, 'recoverPassword'])->name('update.password');
 
