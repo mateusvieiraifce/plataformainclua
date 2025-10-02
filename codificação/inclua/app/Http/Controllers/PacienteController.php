164fe9b478bac6b9bb457949b12e8de37415adab
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2025-10-02 13:12:11 -0300

    subidno cancelamento com taxa.

--- codificação/inclua/app/Http/Controllers/PacienteController.php
+++ codificação/inclua/app/Http/Controllers/PacienteController.php
@@ -755,7 +755,7 @@ class PacienteController extends Controller
       return view('userPaciente.home', ['consultas' => $consultas, 'filtro' => $filtro]);
    }
 
-    public function cancelarConsulta(Request $request)
+    public function cancelarConsultaAnt(Request $request)
     {
         $consulta = Consulta::find($request->consulta_id);
         $consultaController = new ConsultaController();
@@ -830,6 +830,105 @@ class PacienteController extends Controller
            }
         }
         session()->flash('msg', $msg);
+
+        if ($userLogged->tipo_user == "E") {
+            return redirect()->route('consulta.listconsultaporespecialista');
+        } elseif ($userLogged->tipo_user == "P") {
+            return redirect()->route('paciente.minhasconsultas');
+        }
+    }
+    public function cancelarConsulta(Request $request)
+    {
+        $naoCompareceu = false;
+        if($request->compareceu!="Compareceu"){
+            $request->motivo_cancelamento = "Não compareceu";
+            $naoCompareceu = true;
+        }
+
+
+        if ($request->motivo_cancelamento==null || trim($request->motivo_cancelamento) == ""){
+            $msg = ['valor' => trans("Cancelamento não efetivado, um motivo é obrigatório!"), 'tipo' => 'danger'];
+            session()->flash('msg', $msg);
+            return back()->withInput();
+        }
+
+        session()->put("motivo_cancelamento_$request->consulta_id", $request->motivo_cancelamento);
+
+        $consulta = Consulta::find($request->consulta_id);
+        $consultaController = new ConsultaController();
+        $userLogged = Auth::user();
+        if ($consulta->calendarId) {
+            $gc = new GoogleCalendarController();
+            $gc->deleteEvent($consulta->calendarId);
+        }
+
+        if (Helper::verificarPrazoCancelamentoGratuito($consulta->horario_agendado)) {
+            $retornoConsultaCancelada = $consultaController->cancelarConsultaSemTaxa($request);
+        } else {
+            if ($userLogged->tipo_user == "P") {
+                return redirect( route('checkout_stripe',  ["id"=>$consulta->id , "retorno"=>"finalizar.cancelar.paciente"] ));
+            }
+            if ($userLogged->tipo_user == "E") {
+                $this->cancelarFinalizar($consulta->id);
+            }
+        }
+        /* $cartao = Paciente::join('consultas', 'consultas.paciente_id', 'pacientes.id')
+             ->join('users', 'users.id', 'pacientes.usuario_id')
+             ->join('assinaturas', 'assinaturas.user_id', 'users.id')
+             ->join('cartoes', 'cartoes.id', 'assinaturas.cartao_id')
+             ->where('pacientes.id', $consulta->paciente_id)
+             ->select('cartoes.*', 'assinaturas.id as assinatura_id')
+             ->first();
+
+         if ($cartao) {
+             //CRIAR O CHECKOUT
+             $checkout = Helper::createCheckouSumupTaxa();
+             //PASSAR O ID DA CUNSULTA E MOTIVO DE CANCELAMENTO
+             session()->put("consulta_id_$checkout->id", $consulta->id);
+             session()->put("motivo_cancelamento_$checkout->id", $request->motivo_cancelamento);
+             //CRIAR O PAGAMENTO
+             $pagamento = Helper::createPagamento($cartao, $checkout);
+             $pagamentoController = new PagamentoController();
+
+             if (isset($pagamento->status) && $pagamento->status == "FAILED") {
+                 $pagamentoController->store($cartao->user_id, floatval(Helper::converterMonetario(env('TAXA_CANCELAMENTO_CONSULTA'))), $pagamento->transactions[0]->transaction_code, 'Negado', 'Taxa de cancelamento da consulta');
+
+                 return redirect()->route('callback.cancelamento.consulta', ['checkout_id' => $pagamento->id]);
+             } elseif (isset($pagamento->status) && $pagamento->status == "PAID") {
+                 $pagamentoController->store($cartao->user_id, floatval(Helper::converterMonetario(env('TAXA_CANCELAMENTO_CONSULTA'))), $pagamento->transactions[0]->transaction_code, 'Aprovado', 'Taxa de cancelamento da consulta');
+
+                 return redirect()->route('callback.cancelamento.consulta', ['checkout_id' => $pagamento->id]);
+             } elseif (isset($pagamento->next_step)) {
+                 $pagamentoController->store($cartao->user_id, floatval(Helper::converterMonetario(env('TAXA_CANCELAMENTO_CONSULTA'))), $pagamento->next_step->current_transaction->transaction_code, 'Pendente', 'Taxa de cancelamento da consulta');
+
+                 return redirect($pagamento->next_step->url);
+             }
+         } else {
+             if (env('ASSINATURA_OBRIGATORIA')) {
+                 $msg = ['valor' => trans("Nenhum cartão associado!"), 'tipo' => 'danger'];
+             } else {
+                 $consulta = Consulta::find($consulta->id);
+                 $consulta->status="Cancelada";
+                 $consulta->motivocancelamento = $request->motivo_cancelamento;
+                 $consulta->id_usuario_cancelou = Auth::user()->id;
+                 $consulta->save();
+
+                 date_default_timezone_set('America/Sao_Paulo');
+                 $dataConsultaCancelada = Carbon::parse($consulta->horario_agendado);
+                 $dataAtual = Carbon::now();
+
+
+                 if ($dataConsultaCancelada->gt($dataAtual)) {
+                     $consultaNova = $consulta->replicate();
+                     $consultaNova->status = "Disponível";
+                     $consultaNova->paciente_id = null;
+                     $consultaNova->save();
+                 }
+
+
+             }
+        }
+     }*/
 
         if ($userLogged->tipo_user == "E") {
             return redirect()->route('consulta.listconsultaporespecialista');
